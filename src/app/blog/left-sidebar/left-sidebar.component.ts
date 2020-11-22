411ee69a722a3e9a4e57ea680f49fd87c5597b73
SanSoo Han <han@SanSoos-MacBook-Pro.local>
2020-11-23 05:34:23 +0900

    Refactor Blog Structure

--- src/app/blog/left-sidebar/left-sidebar.component.ts
+++ src/app/blog/left-sidebar/left-sidebar.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
+import { FormGroup } from '@angular/forms';
+import { DataTransferHelper } from 'src/app/helper/data-transefer.helper';
+import { RouterHelper } from 'src/app/helper/router.helper';
+import { FormHelper } from 'src/app/helper/form.helper';
 
 @Component({
   selector: 'app-left-sidebar',
@@ -6,10 +11,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['../blog.component.css', './left-sidebar.component.css']
 })
 export class LeftSidebarComponent implements OnInit {
+  @Input() isEditingPost: boolean;
+  @Input() isEditingCategory: boolean;
+  @Input() categoryContentsForm: FormGroup;
 
-  constructor() { }
+  constructor(
+    public authService: AuthService,
+    public dataTransferHelper: DataTransferHelper,
+    public routerHelper: RouterHelper,
+    public formHelper: FormHelper,
+  ) { }
+
+  countChildCategory(categoryId: string) {
+    const count = this.categoryContentsForm.controls.categoryContents.value.filter((categoryContent) =>
+      categoryContent.parentCategoryId === categoryId && categoryContent.id !== categoryId
+    ).length;
+    return count;
+  }
+
+  clickCategoryEdit() {
+    this.isEditingCategory = true;
+  }
+
+  clickCategoryEditCancel(){
+    this.isEditingCategory = false;
+  }
 
   ngOnInit(): void {
   }
-
 }
--- src/app/blog/post/post.component.spec.ts
+++ src/app/blog/post/post.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { EducationComponent } from './education.component';
+import { PostComponent } from './post.component';
 
-describe('EducationComponent', () => {
-  let component: EducationComponent;
-  let fixture: ComponentFixture<EducationComponent>;
+describe('PostComponent', () => {
+  let component: PostComponent;
+  let fixture: ComponentFixture<PostComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ EducationComponent ]
+      declarations: [ PostComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(EducationComponent);
+    fixture = TestBed.createComponent(PostComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
