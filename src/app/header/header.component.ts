d226b89e01e3969a2ed7c0038db25b133cfa2cfb
SanSoo Han <han@SanSoos-MacBook-Pro.local>
2020-12-03 22:16:55 +0900

    Fix Searching Profile and Blog

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -19,6 +19,9 @@ export class HeaderComponent {
   params: any;
   paramSub: Subscription;
 
+  isSearchValueSelected: boolean;
+  selectedSearchName: string;
+
   // tslint:disable-next-line:no-shadowed-variable
   constructor(
     private firestore: AngularFirestore,
@@ -27,31 +30,42 @@ export class HeaderComponent {
     public routerHelper: RouterHelper,
   ) {
     this.searchValue = '';
+    this.isSearchValueSelected = false;
     this.paramSub = this.route.params.subscribe(params => {
       this.isPage = true;
       this.params = params;
     });
   }
 
-  // tslint:disable-next-line:use-lifecycle-interface
-  ngOnInit() {
-
+  closeLinkDropDown(){
+    this.searchValue = '';
+    this.isSearchValueSelected = false;
   }
 
-  changeSearch(event){
+  changeSearch(event: any): void{
+    this.isSearchValueSelected = false;
+    if (!event.key){
+      this.isSearchValueSelected = true;
+      this.searchValue = event.target.value;
+      return;
+    }
+
     if (event.target.value){
       this.searchValue = event.target.value;
       if (this.searchValue === ''){
         this.results = null;
+        return;
       }
-      else{
-        this.results = this.firestore.collection('profiles', ref => ref
-        .orderBy(new firebase.firestore.FieldPath('aboutContent', 'email'))
-        .limit(10)
-        .startAt(this.searchValue)
-        .endAt(this.searchValue + '\uf8ff'))
-        .valueChanges();
-      }
+
+      this.results = this.firestore.collection('profiles', ref => ref
+      .orderBy(new firebase.firestore.FieldPath('aboutContent', 'userName'))
+      .limit(10)
+      .startAt(this.searchValue)
+      .endAt(this.searchValue + '\uf8ff'))
+      .valueChanges();
     }
+  }
+  toggleSearchValueSelected(isSelected: boolean): void{
+    this.isSearchValueSelected = isSelected;
   }
 }
