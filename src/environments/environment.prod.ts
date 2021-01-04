04777cae30604201773c70b2ff72b1d7eccfb75a
SanSoo Han <han@SanSoos-MacBook-Pro.local>
2021-01-04 19:14:51 +0900

    Init Developer Team

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,13 +2,7 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: 'AIzaSyDsi6bLD3hv4ceK43-OMxwaCdqDF9CWMyY',
-    authDomain: 'developers-8a830.firebaseapp.com',
-    databaseURL: 'https://developers-8a830.firebaseio.com',
-    projectId: 'developers-8a830',
-    storageBucket: 'developers-8a830.appspot.com',
-    messagingSenderId: '494786199163',
-    appId: '1:494786199163:web:ae05edd8745969301a2cde',
-    measurementId: 'G-3KS8LEC152'
+    appId: "1:494786199163:web:220ae21813a4f34c1a2cde",
+    measurementId: "G-BHT7WNP5WS"
   }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,14 +5,12 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: 'AIzaSyDsi6bLD3hv4ceK43-OMxwaCdqDF9CWMyY',
-    authDomain: 'developers-8a830.firebaseapp.com',
-    databaseURL: 'https://developers-8a830.firebaseio.com',
-    projectId: 'developers-8a830',
-    storageBucket: 'developers-8a830.appspot.com',
-    messagingSenderId: '494786199163',
-    appId: '1:494786199163:web:ae05edd8745969301a2cde',
-    measurementId: 'G-3KS8LEC152'
+    apiKey: "AIzaSyDsi6bLD3hv4ceK43-OMxwaCdqDF9CWMyY",
+    authDomain: "developers-8a830.firebaseapp.com",
+    databaseURL: "https://developers-8a830.firebaseio.com",
+    projectId: "developers-8a830",
+    storageBucket: "developers-8a830.appspot.com",
+    messagingSenderId: "494786199163",
   }
 };
 
