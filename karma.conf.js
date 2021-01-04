fe0006d270c2d543b0c667e128da6cc5f42a0fd8
SanSoo Han <han@SanSoos-MacBook-Pro.local>
2021-01-04 19:14:51 +0900

    Init Developer Team

--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/developer-new'),
+      dir: require('path').join(__dirname, './coverage/teams'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
--- src/app/view/blog/blog.default.ts
+++
@@ -1,8 +0,0 @@
-import { BlogContent } from './blog.content';
-
-export const blogDefault: BlogContent = {
-  id: '',
-  userName: '',
-  ownerId: '',
-  categoryOrder: [],
-};
